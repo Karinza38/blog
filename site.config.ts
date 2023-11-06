be066c89203da1fce4c9c89168155f0744c12beb
Matthias Koch <ithrowexceptions@gmail.com>
2023-11-06 05:58:38 +0100

    feat: add feed

--- site.config.ts
+++ site.config.ts
@@ -21,7 +21,7 @@ export default siteConfig({
   github: 'matkoch',
   linkedin: 'matthias-koch-jb',
   mastodon: 'https://dotnet.social/@matkoch', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
+  newsletter: 'https://shinycodes.net/feed', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
